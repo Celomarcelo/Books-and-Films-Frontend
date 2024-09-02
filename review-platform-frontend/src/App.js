c98ea6f1abc0e28a72fc388404676c494326e848
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-09-02 12:05:38 +0100

    created navbar
    navbar with home and about links
    search filed

--- review-platform-frontend/src/App.js
+++ review-platform-frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/NavBar';
 import ReviewList from './components/ReviewList';
+import Favorites from './components/Favorites';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<ReviewList />} />
-      </Routes>
+      <Navbar />
+      <div class="container">
+        <div class="row">
+          <Favorites />
+          <Routes>
+            <Route path="/" element={<ReviewList />} />
+          </Routes>
+          <Favorites />
+        </div>
+      </div>
     </Router>
   );
 }
