aa2091540613b8e5f1c2bff667e5b5c90e4bb475
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-09-02 13:13:27 +0100

    added img in reviews component
    added bootstrap classes for page layuot, img field

--- review-platform-frontend/src/components/ReviewList.js
+++ review-platform-frontend/src/components/ReviewList.js
@@ -3,26 +3,36 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 const ReviewList = () => {
-  const [reviews, setReviews] = useState([]);
+    const [reviews, setReviews] = useState([]);
 
-  useEffect(() => {
-    axios.get('/api/reviews/')
-      .then(response => setReviews(response.data))
-      .catch(error => console.error(error));
-  }, []);
+    useEffect(() => {
+        axios.get('/api/reviews/')
+            .then(response => setReviews(response.data))
+            .catch(error => console.error(error));
+    }, []);
 
-  return (
-    <div>
-      <h1>Resenhas de Livros e Filmes</h1>
-      <ul>
-        {reviews.map(review => (
-          <li key={review.id}>
-            <Link to={`/reviews/${review.id}`}>{review.title}</Link> - {review.author_director}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div class="col-md-8 text-center">
+            <h1>Resenhas de Livros e Filmes</h1>
+            <ul>
+                {reviews.map(review => (
+                    <li key={review.id}>
+                        {review.img && (
+                            <img
+                                src={review.img}
+                                alt={review.title}
+                                className="img-fluid mb-2"
+                                style={{ maxWidth: '200px', height: 'auto' }}
+                            />
+                        )}
+                        <div>
+                            <Link to={`/reviews/${review.id}`}>{review.title}</Link> - {review.author_director}
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default ReviewList;
