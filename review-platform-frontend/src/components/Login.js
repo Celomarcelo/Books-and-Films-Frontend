7798da1605ae7ee18cd5c148c8229eaa3618db33
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-09-06 20:05:05 +0100

    created login.js
    cretaed const login with username and password
    return html with divs, form for login and password

---
+++ review-platform-frontend/src/components/Login.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('/api/login/', {
+            username,
+            password,
+        })
+            .then(response => {
+                localStorage.setItem('token', response.data.access);
+                navigate('/');
+            })
+            .catch(error => console.error(error));
+    };
+
+    return (
+        <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+            <form className="w-100 p-3" onSubmit={handleSubmit}>
+                <div className="w-100 bg-secondary rounded p-4">
+                    <div className="text-center mb-4">
+                        <h2 className="fs-1">Login</h2>
+                    </div>
+                    <div className="mb-3">
+                        <input
+                            type="text"
+                            className="form-control form-control-lg"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <input
+                            type="password"
+                            className="form-control form-control-lg"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="d-grid">
+                        <button type="submit" className="btn btn-primary btn-lg">
+                            Login
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
