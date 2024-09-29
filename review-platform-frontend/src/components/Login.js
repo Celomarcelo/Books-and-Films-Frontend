67c8ad8286a1d1872a20c00c9206bb31098f1500
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-09-29 12:21:48 +0100

    added error and description to login
    added const passwordError to display error
    added div description aside login div

--- review-platform-frontend/src/components/Login.js
+++ review-platform-frontend/src/components/Login.js
@@ -8,6 +8,7 @@ const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
+    const [passwordError, setError] = useState(null);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -21,14 +22,20 @@ const Login = () => {
                 navigate('/profile/');
             })
             .catch(error => console.error("Login Error:", error));
+            setError('Enter with a valid ID or password.');
     };
 
     return (
         <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+            <div className="text-center" style={{ marginRight: '150px' }}>
+                <h1>Books&Films</h1>
+                <p>Share ideas about books and films!</p>
+            </div>
             <form className="p-3" onSubmit={handleSubmit}>
                 <div className="w-100 bg-secondary rounded p-4">
                     <div className="text-center mb-4">
                         <h2 className="fs-1">Login</h2>
+                        {passwordError && <div className="alert alert-danger mt-3 text-center">{passwordError}</div>}
                     </div>
                     <div className="mb-3">
                         <input
